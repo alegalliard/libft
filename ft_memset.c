85bc9937ca897dda5aafbbe1122a3232f8883519
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-18 21:44:16 -0300

    adicionando 42 header

--- ft_memset.c
+++ ft_memset.c
@@ -2,13 +2,9 @@
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
+	while (n--)
 	{
-		((unsigned char *)str)[i] = c;
-		i++;
+		((unsigned char *)str)[n] = c;
 	}
 	return (str);
 }
