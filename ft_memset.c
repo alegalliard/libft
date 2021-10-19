136182c5a401a36542c271fcd5af0374ecbcd1c6
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-18 22:04:33 -0300

    memset, bzero

--- ft_memset.c
+++ ft_memset.c
@@ -1,10 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 21:45:26 by alegalliard       #+#    #+#             */
+/*   Updated: 2021/10/18 22:04:08 by alegalliard      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	while (n--)
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		((unsigned char *)str)[n] = c;
+		((unsigned char *)str)[i] = c;
+		i++;
 	}
-	return (str);
+	return ((unsigned char *)str);
 }
