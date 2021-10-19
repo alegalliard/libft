df33e4d1a8574c23c33d9a684a5ac5c776974e7f
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-18 22:23:08 -0300

    memcpy com header

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 22:09:40 by alegalliard       #+#    #+#             */
+/*   Updated: 2021/10/18 22:22:43 by alegalliard      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include	"libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -10,5 +22,5 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return (dest);
+	return ((unsigned char *)dest);
 }
