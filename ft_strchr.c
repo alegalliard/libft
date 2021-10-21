35b353def2880f9ed29abb7e1fbaa2a2cc225c0a
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-20 22:31:55 -0300

    corrigindo strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,14 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 22:31:13 by alegalliard       #+#    #+#             */
+/*   Updated: 2021/10/20 22:31:37 by alegalliard      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
+	if (c == '\0')
+		return ((char *)s + ft_strlen(s));
+	if (!ft_isascii(c))
+		return ((char *)s);
 	while (s[i])
 	{
 		if (s[i] == c)
-			return ((char *)s);
+			return ((char *)s + i);
 		i++;
 	}
 	return (NULL);
