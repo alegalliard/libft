d33811dc84dbaabb4ffa4cafc3b8e0a1773b7886
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-20 23:20:16 -0300

    headers em funções de memória

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 22:46:32 by alegalliard       #+#    #+#             */
-/*   Updated: 2021/10/20 22:46:33 by alegalliard      ###   ########.fr       */
+/*   Updated: 2021/10/20 23:00:13 by alegalliard      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
-	int found_at;
+	int	found_at;
 
 	i = 0;
 	found_at = -1;
@@ -29,7 +29,7 @@ char	*ft_strrchr(const char *s, int c)
 			found_at = i;
 		i++;
 	}
-	if(found_at >= 0)
+	if (found_at >= 0)
 		return ((char *)s + found_at);
 	return (NULL);
 }
