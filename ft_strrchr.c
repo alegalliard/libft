1ed5f891eb7b1cf35ef74fa82a7f76ba1dd5979d
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-20 22:58:37 -0300

    reescrevendo a strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,17 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 22:46:32 by alegalliard       #+#    #+#             */
+/*   Updated: 2021/10/20 22:46:33 by alegalliard      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	i;
-	char	*last;
+	int	i;
+	int found_at;
 
 	i = 0;
-	last = NULL;
+	found_at = -1;
+	if (c == '\0')
+		return ((char *)s + ft_strlen(s));
+	if (!ft_isascii(c))
+		return ((char *)s);
 	while (s[i])
 	{
 		if (s[i] == c)
-			last = ((char *)s);
+			found_at = i;
 		i++;
 	}
-	return (last);
+	if(found_at >= 0)
+		return ((char *)s + found_at);
+	return (NULL);
 }
