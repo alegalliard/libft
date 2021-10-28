7e2590a396060e8c6fdfb1c2f1fe7850e1c7dbe4
Alessandra Dias de Souza <adias-de@student.42sp.org.br>
2021-10-28 08:43:21 -0300

    parcial - bug na substr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 07:46:33 by alegalliard       #+#    #+#             */
-/*   Updated: 2021/10/28 07:46:34 by alegalliard      ###   ########.fr       */
+/*   Updated: 2021/10/28 08:17:25 by alegalliard      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	void	*dest;
 	size_t	mem_size;
-	void	*arr;
-
 	mem_size = count * size;
-	arr = malloc(mem_size);
-	if (!arr)
+	
+	if (!(dest = malloc(mem_size)))
 		return (NULL);
-	ft_memset(arr, 0, mem_size);
-	return (arr);
+	ft_memset(dest, 0, mem_size);
+
+	return ((void *)dest);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -1,22 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegalliard <alegalliard@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 08:42:26 by alegalliard       #+#    #+#             */
+/*   Updated: 2021/10/28 08:42:27 by alegalliard      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*sub;
-	unsigned int	s_len;
 	size_t			i;
-	size_t			j;
-
-	if (!s || !len)
-		return (NULL);
-	s_len = ft_strlen(s);
-	sub = malloc((len + 1) * sizeof(char));
-	if (!sub || start > s_len)
-		return (NULL);
-	i = start;
-	j = 0;
-	while (s[i] && j < len)
-		sub[j++] = s[i++];
-	sub[j] = '\0';
+/* aloca e retorna uma substring da string s
+a substring inicia no start e tem o tamanho máximo de len
+*/
+	if(!(sub = malloc((len + 1) * sizeof(char))))
+		return NULL;
+	i = 0;
+	while(len && s[i])
+	{
+		sub[i++] = s[start++]; //tá dando overlap
+		len--;
+	}
+	sub[i] = '\0';
 	return (sub);
 }
